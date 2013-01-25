c1d4e472a70449df42d034647bcfa6e0882f547d
Brian Graham <drakain@gmail.com>
2013-01-25 09:07:21 -0500

    Outline basic first steps

--- 001/game.js
+++ 001/game.js
@@ -1,3 +1,23 @@
+/*
+ * Tasks:
+ *   Add game object
+ *   Add item render functions
+ *   Add motion logic
+ *   Bind motion to render
+ **/
+
+
+
+(function() {
+    var game = function() {
+    };
+
+    game.prototype.foo = function() {
+    }
+
+});
+
+
 (function() {
     var canvasElement = document.getElementById("game");
     var canvas = canvasElement.getContext("2d");
@@ -12,7 +32,17 @@
         }, 1000/FPS);
     };
 
-    function draw() {
+
+    function draw(displayItems) {
+        var item;
+        for (item in displayItems) {
+            if (displayItems.hasOwnProperty(item)) {
+               //TODO: This doesn't keep track of z-index order.
+               item.render();
+            }
+        }
+
+
         canvas.fillStyle = "rgba(0, 0, 200, 0.1)"; // Set color to black
         canvas.fillRect(190, 0, 520, 520);
 
@@ -23,6 +53,7 @@
     }
 
     function update() {
+
     }
 
 })();
